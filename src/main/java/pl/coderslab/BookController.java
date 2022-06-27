9adcac98f31c8b14f3e6479816d02bf27071db58
arek jozwiak <arkadiusz.jozwiak@coderslab.pl>
2022-06-27 14:45:44 +0200

    day 1

---
+++ src/main/java/pl/coderslab/BookController.java
@@ -0,0 +1,44 @@
+package pl.coderslab;
+
+import org.springframework.stereotype.Controller;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.ResponseBody;
+
+@Controller
+@RequestMapping("/book")
+public class BookController {
+
+    private final  BookDao bookDao;
+    private final PublisherDao publisherDao;
+
+    public BookController(BookDao bookDao, PublisherDao publisherDao) {
+        this.bookDao = bookDao;
+        this.publisherDao = publisherDao;
+    }
+
+    @RequestMapping("/test")
+    @ResponseBody
+    public String test(){
+        Book byId = bookDao.findById(1l);
+        byId.setTitle(" Other book about java");
+        bookDao.update(byId);
+        return "test";
+    }
+
+    @RequestMapping("/add")
+    @ResponseBody
+    public String add(){
+        Publisher publisher = new Publisher();
+        publisher.setName("Helion");
+        publisherDao.save(publisher);
+
+        Book book = new Book();
+        book.setPublisher(publisher);
+        book.setTitle("Java 2 podstawy");
+        book.setRating(5);
+        bookDao.save(book);
+
+        return "test";
+    }
+
+}
--- src/main/java/pl/coderslab/Publisher.java
+++ src/main/java/pl/coderslab/Publisher.java
@@ -4,19 +4,15 @@ import lombok.Getter;
 import lombok.Setter;
 
 import javax.persistence.*;
+import java.util.List;
 
 @Entity
 @Getter
 @Setter
-@Table(name = "books")
-public class Book {
+@Table(name = "publishers")
+public class Publisher {
     @Id
     @GeneratedValue(strategy = GenerationType.IDENTITY)
     private Long id;
-    private String title;
-    private int rating;
-    private String description;
-    @ManyToOne
-    private Publisher publisher;
-
+    private String name;
 }
