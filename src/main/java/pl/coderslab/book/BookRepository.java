920f02b2bdfcd3af28e80d0205221298727fd975
arek jozwiak <arkadiusz.jozwiak@coderslab.pl>
2022-06-29 16:19:57 +0200

    day 3

--- src/main/java/pl/coderslab/book/BookRepository.java
+++ src/main/java/pl/coderslab/book/BookRepository.java
@@ -1,6 +1,7 @@
 package pl.coderslab.book;
 
 import org.springframework.data.jpa.repository.JpaRepository;
+import org.springframework.data.jpa.repository.Query;
 import org.springframework.stereotype.Repository;
 
 import java.util.List;
@@ -9,10 +10,21 @@ import java.util.List;
 public interface BookRepository extends JpaRepository<Book, Long> {
 
     List<Book> findAllByRating(int rating);
+
     List<Book> findAllByRatingGreaterThan(int rating);
 
     List<Book> findByTitle(String title);
+
     List<Book> findByCategory(Category category);
+
+    List<Book> findByCategoryAndPublisherAndRating(Category category, Publisher publisher, int rating);
+
     List<Book> findByCategoryId(Long id);
+
+    @Query("select b from Book b where b.title=?1")
+    List<Book> findByTitleQuery(String title);
+
+    @Query("select b from Book b where b.category=?1 and b.publisher=?2")
+    List<Book> findByCategoryQuery(Category category, Publisher publisher);
 
 }
